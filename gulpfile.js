3b125360f3457f5b0a032a97261fc497cb601172
toybreaker <rokmabox@gmail.com>
2016-02-03 03:02:08 +0100

    details fixing

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var sass = require('gulp-sass');
 var uncss = require('gulp-uncss');
 
 gulp.task('u', function () {
-    return gulp.src('/main.css')
+    return gulp.src('main.css')
         .pipe(uncss({
-            html: ['/index.htm']
+            html: ['index.htm']
         }))
-        .pipe(gulp.dest('/uncss/'));
+        .pipe(gulp.dest('./uncss/'));
 });
 
 gulp.task('sass', function () {
@@ -20,28 +20,3 @@ gulp.task('sass', function () {
 gulp.task('default', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
-
-
-// var gulp = require('gulp');
-// var sass = require('gulp-sass');
-// var uncss = require('gulp-uncss');
-//
-//
-// gulp.task('default', function () {
-//   return gulp.src('./src/sass/**/*.sass')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('./src/css'));
-// });
-//
-// gulp.task('watch', function () {
-//   gulp.watch('./src/sass/**/*.sass', ['sass']);
-// });
-//
-//
-// gulp.task('uncss', function () {
-//     return gulp.src('/src/css/main.css')
-//         .pipe(uncss({
-//             html: ['/src/index.htm']
-//         }))
-//         .pipe(gulp.dest('./scr/uncss/'));
-// });
